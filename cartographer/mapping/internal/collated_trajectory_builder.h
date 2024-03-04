994fd85776ff57b561ee8eb6023a90605f958885
devaiah.arun <devaiah.arun@ubica-robotics.eu>
2024-03-04 13:56:58 +0100

    [DEV-8095] Handles adaptive scan matching data in collated trajectory builder

--- cartographer/mapping/internal/collated_trajectory_builder.h
+++ cartographer/mapping/internal/collated_trajectory_builder.h
@@ -67,6 +67,11 @@ class CollatedTrajectoryBuilder : public TrajectoryBuilderInterface {
     AddData(sensor::MakeDispatchable(sensor_id, odometry_data));
   }
 
+  void AddSensorData(const std::string& sensor_id,
+                   const sensor::AdaptiveScanMatchingData& adaptive_scan_matching_data) override {
+    AddData(sensor::MakeDispatchable(sensor_id, adaptive_scan_matching_data));
+  }
+
   void AddSensorData(
       const std::string& sensor_id,
       const sensor::FixedFramePoseData& fixed_frame_pose_data) override {
